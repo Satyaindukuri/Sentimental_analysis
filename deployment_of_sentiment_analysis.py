2a54895fd07ee15050dc5126ba9f0f59d45ba6fa
Satyaindukuri <129823389+Satyaindukuri@users.noreply.github.com>
2023-09-09 16:27:29 +0530

    Update deployment_of_sentiment_analysis.py

--- deployment_of_sentiment_analysis.py
+++ deployment_of_sentiment_analysis.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1Mbeh_SQxp1MxprFG_8TCwOMqv_PALEWY
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
